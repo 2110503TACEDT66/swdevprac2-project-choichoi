77f89c57df210b1cb98f7b0d08c6d519bbc77ee7
Sippapas Charoenkul <juei2586sc@gmail.com>
2024-03-26 15:54:22 +0700

    revert bookinglist

--- src/components/BookingList.tsx
+++ src/components/BookingList.tsx
@@ -1,13 +1,13 @@
-import getReservations from "@/libs/getReservations";
+"use client"
 import { addBooking, removeBooking } from "@/redux/features/bookSlice";
 import { AppDispatch, useAppSelector } from "@/redux/store"
 import { Button } from "@mui/material";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-export default async function BookingList(){
-    /*const bookItems = await getReservations();
-
+export default function BookingList(){
+    const bookItems = useAppSelector((state)=> state.bookSlice.bookItems);
+    const dispatch = useDispatch<AppDispatch>()
     return (
         <div>
             {
@@ -19,10 +19,10 @@ export default async function BookingList(){
                         <div>Citizen ID: {bookItem.id}</div>
                         <div>Hospital: {bookItem.hospital}</div>
                         <div>Booking Date: {bookItem.bookDate}</div>
-                        <Button className="block rounded-md bg-sky-600 hover:bg-indigo-600 px-3 py-1 text-white shadow-sm" onClick={()=>}>remove booking</Button>
+                        <Button className="block rounded-md bg-sky-600 hover:bg-indigo-600 px-3 py-1 text-white shadow-sm" onClick={()=>dispatch(removeBooking(bookItem.id))}>remove booking</Button>
                     </div>
                 )):<div>No Vaccine Booking</div>
             }
         </div>
-    );*/
+    );
 }
