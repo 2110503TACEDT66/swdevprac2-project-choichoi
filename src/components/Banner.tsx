614927aa72c71918224ffb5687c873227e3f8a57
Sippapas Charoenkul <juei2586sc@gmail.com>
2024-03-26 22:17:35 +0700

    css main page

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -15,10 +15,9 @@ export default function Banner () {
         <div className="block w-screen h-[80vh] relative m-0 p-[5px]">
             <Image src={"/img/cover.png"}  alt="COVER" priority fill={true} objectFit='cover'/>
             <div className="relative z-20 text-center bg-[rgba(255,255,255,0.5)] p-2.5 top-[100px]">
-                <h1 className='text-4xl font-medium'>Vaccine Service Center</h1>
-                <h3 className='text-xl font-serif'>Your health matters, Join us for free vaccine</h3>
+                <h1 className='text-4xl font-medium'>Choi Choi</h1>
+                <h3 className='text-xl font-serif'>Massage Reservation</h3>
             </div>
-            <Button variant='outlined' className=' absolute right-5 bottom-5 bg-white hover:bg-slate-200' onClick={()=>{router.push("/hospital")}}>Select Hospital</Button>
         </div>
     );
 } 
--- src/components/PromoteCard.tsx
+++ src/components/PromoteCard.tsx
@@ -9,10 +9,10 @@ export default function PromoteCard(){
 
     const [playing,setPlaying] = useState(true);
     return(
-        <div className="w-[80%] shadow-lg mx-[10%] my-10 p-2 rounded-lg bg-gray-200 flex flex-row">
+        <div className="w-[80%] shadow-lg mx-[10%] my-10 p-2 rounded-lg bg-[#c6c2c2] flex flex-row">
             <VideoPlayer vdoSrc="/vdo/getvaccine.mp4" isPlaying={playing}/>
             <div className="m-5">
-                Get your vaccine today.
+                Free Massage Anywhere Anytime!!!
                 <button className="block rounded-md bg-sky-600 hover:bg-indigo-600 px-3 py-2 text-white shadow-sm mt-10" onClick={()=>{setPlaying(!playing)}}>
                     {playing ? "Pause":"Play"}
                 </button>
