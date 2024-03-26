433e952faf1e15a5c9c9ebe333de19e3634b38c4
Sippapas Charoenkul <juei2586sc@gmail.com>
2024-03-26 22:53:44 +0700

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import styles from './page.module.css'
 import Banner from '@/components/Banner'
 import Card from '@/components/Card'
-import CardPanel from '@/components/CardPanel'
 import PromoteCard from '@/components/PromoteCard'
 
 export default function Home() {
