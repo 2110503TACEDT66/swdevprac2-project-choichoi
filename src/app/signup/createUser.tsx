cd4875dab4ba9cf4339bd36bef62aae8ebe2af46
Imnotmetch <main.thanametch.klao@gmail.com>
2024-03-26 18:33:30 +0700

    fix signup

--- src/app/signup/createUser.tsx
+++ src/app/signup/createUser.tsx
@@ -1,6 +1,7 @@
 'use server'
 import { dbConnect } from "@/db/dbConnect"
 import User from "@/db/models/User"
+import { redirect } from "next/dist/server/api-utils"
 
 export const createUser = async (name:string,tel:string,email:string,password:string,role:string) => {
     
